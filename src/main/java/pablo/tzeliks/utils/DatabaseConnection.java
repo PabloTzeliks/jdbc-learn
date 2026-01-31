8cf1f3525a5c1b890211bd8ad643e89201ff26db
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-31 17:25:35 -0300

    feat: add Connection class for JDBC

---
+++ src/main/java/pablo/tzeliks/utils/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package pablo.tzeliks.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/simple_db";
+    private static final String USER = "db_user";
+    private static final String PASS = "db_pass";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER, PASS);
+            }
+            return connection;
+
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database. Check: " + e.getMessage());
+
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
